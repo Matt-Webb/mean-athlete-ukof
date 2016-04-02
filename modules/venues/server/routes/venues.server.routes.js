391aa9b7834de0d466e0a5850f1c83739a090577
Matthew D Webb <matt.d.webb@icloud.com>
2016-04-02 15:11:57 +0100

    configurations for venue by city routes

--- modules/venues/server/routes/venues.server.routes.js
+++ modules/venues/server/routes/venues.server.routes.js
@@ -4,17 +4,22 @@
  * Module dependencies.
  */
 var venuesPolicy = require('../policies/venues.server.policy'),
-  venues = require('../controllers/venues.server.controller');
+    venues = require('../controllers/venues.server.controller');
 
-module.exports = function (app) {
-  // Venues collection routes
-  app.route('/api/venues').all(venuesPolicy.isAllowed)
-    .get(venues.list);
+module.exports = function(app) {
+    // Venues collection routes
+    app.route('/api/venues').all(venuesPolicy.isAllowed)
+        .get(venues.list);
 
-  // Single venue routes
-  app.route('/api/venues/:venueId').all(venuesPolicy.isAllowed)
-    .get(venues.read);
+    // Single venue routes
+    app.route('/api/venues/:city').all(venuesPolicy.isAllowed)
+        .get(venues.venueByCity);
 
-  // Finish by binding the venue middleware
-  app.param('venueId', venues.venueByID);
+    // Single venue routes
+    app.route('/api/venues/:venueId').all(venuesPolicy.isAllowed)
+        .get(venues.read);
+
+    // Finish by binding the venue middleware
+    app.param('venueId', venues.venueByID);
+    app.param('city', venues.venueByCity);
 };
